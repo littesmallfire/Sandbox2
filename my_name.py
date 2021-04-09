de567e3182845a532e1401931a380f816960db18
kangleng <kangleng@gmail.com>
2021-04-09 09:41:43 +0800

    Check whether the user is an adult

--- my_name.py
+++ my_name.py
@@ -5,6 +5,15 @@ def main():
     name = input("Name? ")
     age = int(input("Age? "))
     print("Your name is {} and age is {}".format(name, age))
+    if is_adult(age):
+        print("You are an adult")
+    else:
+        print("You are a child")
+
+
+def is_adult(age):
+    return age >= 21
+
 
 main()
 
