3bc3aa4fef24a18221e488c8533d2c6863a80152
kangleng <kangleng@gmail.com>
2021-04-09 09:37:00 +0800

    Add user input in main

--- my_name.py
+++ my_name.py
@@ -2,7 +2,14 @@
 
 
 def main():
-    print("My name is Charlie Brown")
-
+    name = input("Name? ")
+    age = int(input("Age? "))
+    print("Your name is {} and age is {}".format(name, age))
 
 main()
+
+### 1st step -> commit
+### 2nd step -> push
+
+### commit is local
+### push is remote
